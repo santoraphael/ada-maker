1edfa6f3a92f747728ae082a01b6c650baf9eb7a
Raphael Santo <raphael.esanto@gmail.com>
2024-11-27 00:52:34 -0300

    commit inicial

---
+++ ADAMAKER/website/Controllers/VisionMissionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace website.Controllers
+{
+    public class VisionMissionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
