2d9680f7300f81de6f6c6371b4732be34156de7b
Raphael Santo <raphael.esanto@gmail.com>
2024-11-30 14:42:49 -0300

    Varias paginas

---
+++ ADAMAKER/website/Controllers/RoadmapController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace website.Controllers
+{
+    public class RoadmapController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
